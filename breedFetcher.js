d861aea1e60a44e493e18491c9f8b5798e94b713
AD202310 <a.g.dychenko@gmail.com>
2023-12-31 21:25:39 -0500

    commit changes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,13 @@
 const request = require('request');
 let input = process.argv[2];
-let search = input.substr(0, 4);
+let search = input[0] + input[1] + input[2] + input[3];
+// let search = input.substr(0, 4); - show error for some reason
 let url = `https://api.thecatapi.com/v1/breeds/search?q=${search}`;
-request(url, function(error, response, body) {
-  console.log('error:', "This is not a cat breed!")
+
+const breedFetcher = request(url, function(error, response, body) {
   const data = JSON.parse(body);
   console.log(data[0].description);
 });
 
+module.exports = { fetchBreedDescription };
 
